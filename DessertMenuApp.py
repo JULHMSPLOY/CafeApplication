430ec61129c9a3eb03f25a5147339f4b28df1bce
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-01-15 17:23:32 +0700

    Menu

--- DessertMenuApp.py
+++ DessertMenuApp.py
@@ -1,11 +1,13 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.uix.label import Label
 class DessertMenu(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.orientation = 'vertical'
 
+        self.add_widget(Label(text = 'Menu', font_size = 40, size_hint = (1, 0.1)))
+        
 class DessertMenuApp(App):
     def build(self):
         return DessertMenu()
