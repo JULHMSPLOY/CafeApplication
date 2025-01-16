b27246d254dc93a933e67f0d0643cbf1c11e0d5d
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-01-16 23:05:44 +0700

    Add GridLayout and Bind

--- DessertMenuApp.py
+++ DessertMenuApp.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
 
 class DessertMenu(BoxLayout):
     def __init__(self, **kwargs):
@@ -10,6 +11,8 @@ class DessertMenu(BoxLayout):
 
         self.add_widget(Label(text = 'Menu', font_size = 40, size_hint = (1, 0.1)))
         self.scroll_view = ScrollView(size_hint = (1, 0.5))
+        self.menu = GridLayout(cols = 1, spacing = 5, size_hint_y = None)
+        self.menu.bind(minimum_height = self.menu.setter('height'))
 
         self.desserts = [
             {"name": "Chocolate Cake"},
