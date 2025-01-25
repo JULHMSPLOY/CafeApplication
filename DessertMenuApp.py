4ef8d2f5803c3371adbdd875defa1ac2b5998bfb
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-01-25 18:17:56 +0700

    Add Spinner Widget To Select Dessert Quantity And Update Cart

--- DessertMenuApp.py
+++ DessertMenuApp.py
@@ -6,6 +6,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
+from kivy.uix.spinner import Spinner
 
 class DessertMenu(BoxLayout):
     def __init__(self, **kwargs):
@@ -39,6 +40,8 @@ class DessertMenu(BoxLayout):
                          {"Name": "Sugar Rush Donut", "image": "sugar_rush_donut.jpg", "Price": "35.00 Bath"}]
         
         self.buttons = []
+        self.cart = []
+        
         for dessert in self.desserts:
             dessert_layout = BoxLayout(orientation = 'vertical', size_hint_y = None, height = 200)
 
@@ -49,8 +52,14 @@ class DessertMenu(BoxLayout):
             btn.bind(on_press = self.add_to_cart)
             dessert_layout.add_widget(btn)
 
+            count_spinner = Spinner(
+                text='1', values=[str(i) for i in range(1, 11)], size_hint=(1, None), height=30
+            )
+            count_spinner.bind(text=lambda spinner, value, dessert_name=dessert["Name"]: self.set_quantity(dessert_name, int(value)))
+            dessert_layout.add_widget(count_spinner)
+
             self.menu.add_widget(dessert_layout)
-            self.buttons.append((btn, dessert["Name"], dessert["Price"]))
+            self.buttons.append((btn, dessert["Name"], dessert["Price"], count_spinner))
 
         self.scroll_view.add_widget(self.menu)
         self.add_widget(self.scroll_view)
@@ -69,33 +78,43 @@ class DessertMenu(BoxLayout):
         self.reset_button.bind(on_press = self.reset_cart)
         self.add_widget(self.reset_button)
 
-        self.cart = []
-
     def search_desserts(self, instance, value):
         for btn, dessert_name, _ in self.buttons:
             btn.parent.opacity = 1 if value.lower() in dessert_name.lower() else 0
         
     def add_to_cart(self, instance):
-        for btn, dessert_name, dessert_price in self.buttons:
+        for btn, dessert_name, dessert_price, count_spinner in self.buttons:
             if instance == btn:
                 price = float(dessert_price.split()[0])
-                self.cart.append({"Name": dessert_name, "Price": price})
+                quantity = int(count_spinner.text)
+                self.cart.append({"Name": dessert_name, "Price": price, "Quantity": quantity})
                 self.update_cart_label()
                 self.calculate_total_price()
                 break
+    def set_quantity(self, dessert_name, quantity):
+        for item in self.cart:
+            if item["Name"] == dessert_name:
+                item["Quantity"] = quantity
+                break
+        else:
+            price = float([d['Price'].split()[0] for d in self.desserts if d["Name"] == dessert_name][0])
+            self.cart.append({"Name": dessert_name, "Price": price, "Quantity": quantity})
+        self.update_cart_label()
+        self.calculate_total_price()
 
     def update_cart_label(self):
-        self.cart_label.text = f"Cart: {len(self.cart)} items"
+        total_items = sum(item["Quantity"] for item in self.cart)
+        self.cart_label.text = f"Cart: {total_items} items"
 
     def calculate_total_price(self):
-        total_price = sum(float(item["Price"]) for item in self.cart)
+        total_price = sum(float(item["Price"] * item["Quantity"]) for item in self.cart)
         self.total_label.text = f"Total: {total_price:.2f} Bath"
 
     def checkout(self, instance):
         print("Checking out the following items: ")
         for item in self.cart:
-            print(f"- {item['Name']} - {item['Price']} Bath")
-        total_price = sum(item["Price"] for item in self.cart)
+            print(f"- {item['Name']} x {item['Quantity']} - {item['Price']} Bath")
+        total_price = sum(item["Price"] * item["Quantity"] for item in self.cart)
         print(f"Total amount: {total_price:.2f} Bath")
         self.cart = []
         self.update_cart_label()
